c1f87afe276b5cddd0228cff96bd281d1137c8fa
Gino Heyman <gino.heyman@gmail.com>
2013-12-15 11:23:17 +0100

    add katana test project

---
+++ src/KatanaTest/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(KatanaTest.Startup))]
+
+namespace KatanaTest
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Use((context, next) =>
+            {
+                PrintCurrentIntegratedPipelineStage(context, "Middleware 1");
+                return next.Invoke();
+            });
+            app.Use((context, next) =>
+            {
+                PrintCurrentIntegratedPipelineStage(context, "2nd MW");
+                return next.Invoke();
+            }); 
+            app.Run(context =>
+            {
+                PrintCurrentIntegratedPipelineStage(context, "3rd MW");
+                return context.Response.WriteAsync("Hello world");
+            });            
+        }
+        private void PrintCurrentIntegratedPipelineStage(IOwinContext context, string msg)
+        {
+            var currentIntegratedpipelineStage = HttpContext.Current.CurrentNotification;
+            context.Get<TextWriter>("host.TraceOutput").WriteLine(
+                "Current IIS event: " + currentIntegratedpipelineStage
+                + " Msg: " + msg);
+        }
+    }
+
+}
