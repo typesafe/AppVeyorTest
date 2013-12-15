8be16b1fc082f006d59b1314ce2497c849a10f04
Gino Heyman <gino.heyman@gmail.com>
2013-12-15 12:42:41 +0100

    replace deploy script with test script

--- deployment/deploy.ps1
+++ deployment/deploy.ps1
@@ -1,45 +1,28 @@
 Param (
-    $configurationName,
-    $variables = @{},
+    $variables = @{},   
     $artifacts = @{},
+    $scriptPath,
+    $buildFolder,
+    $srcFolder,
+    $outFolder,
+    $tempFolder,
     $projectName,
-    $projectVersion
+    $projectVersion,
+    $projectBuildNumber
 )
 
-$scriptsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
-
-# configure deployment
-. (Join-Path $scriptsPath "configure.ps1") $configurationName
-
-# perform deployment
-$environment = $variables.Environment
-if($environment)
+# list all artifacts
+foreach($artifact in $artifacts.values)
 {
-    Write-Host "Deploying to environment: $environment"
-
-    # does specified environment exist?
-    Get-Environment -Name $environment | Out-Null
-
-    # deploy AppRolla application
-    New-Deployment $projectName $projectVersion -To $environment
+    Write-Output "Artifact: $($artifact.name)"
+    Write-Output "Type: $($artifact.type)"
+    Write-Output "Path: $($artifact.path)"
+    Write-Output "Url: $($artifact.url)"
 }
 
-# perform Azure deployment
-$azureEnvironment = $variables.AzureEnvironment
-if($azureEnvironment)
+# script custom variables
+foreach($name in $variables.keys)
 {
-    Write-Host "Deploying to Azure CS environment: $azureEnvironment"
-
-    # does specified environment exist?
-    Get-Environment -Name $azureEnvironment | Out-Null
-
-    # deploy first Azure application to the selected environment
-    foreach($app in Get-Application)
-    {
-        if($app.Type -eq "Azure")
-        {
-            New-Deployment $app.Name $projectVersion -To $azureEnvironment
-            break
-        }
-    }
+    $value = $variables[$name]
+    Write-Output "$name=$value"
 }
